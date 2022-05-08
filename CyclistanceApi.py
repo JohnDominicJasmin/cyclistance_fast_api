4b3a053db35acee00ee77b903f26472712e887b5
John_Dominic <johndominicjasmin@gmail.com>
2022-05-09 00:53:43 +0800

    Add to heroku

--- CyclistanceApi.py
+++ CyclistanceApi.py
@@ -39,7 +39,7 @@ def get_user_by_id(user_id: str):
     for item_id in users: 
         if users[item_id].name == user_id: 
             return users[item_id]
-    raise HTTPException(status_code=404, description="User not found")
+    raise HTTPException(status_code=404, detail="User not found")
 
 @app.get("/get-users")
 def get_users():
@@ -48,7 +48,7 @@ def get_users():
 @app.post("/create-user")
 def create_user(user: User):
     if user.id in users:
-        raise HTTPException(status_code=409, description="User already exist")
+        raise HTTPException(status_code=409, detail="User already exist")
     users[user.id] = user
     return users[user.id]
         
@@ -56,7 +56,7 @@ def create_user(user: User):
 def update_user(item_id:str, user:UpdateUser):
 
     if item_id not in users:
-        raise HTTPException(status_code=404, description="User not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
     if user.name != None:
         users[item_id].name = user.name
@@ -72,7 +72,7 @@ def update_user(item_id:str, user:UpdateUser):
 @app.delete("/delete-user")
 def delete_user(item_id:str = Query(..., description = "The id item to delete.")): 
     if item_id not in users:
-        raise HTTPException(status_code=404, description="User not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
     del users[item_id]
     return {"Success":"Item Deleted!"}
@@ -122,7 +122,7 @@ def get_assistance_by_id(user_id: str):
     for item_id in users:
         if users_assistance[item_id].id == user_id:
             return users_assistance[item_id]
-    raise HTTPException(status_code=404, description="User not found!")
+    raise HTTPException(status_code=404, detail="User not found!")
 
 @app.get("/get-user-assistance")
 def get_assistance(): 
@@ -131,7 +131,7 @@ def get_assistance():
 @app.post("/create-user-assistance")
 def create_user_assistance(assistance: UserAssistance):
     if assistance.id in users_assistance:
-        raise HTTPException(status_code=409, description="User already exists!")
+        raise HTTPException(status_code=409, detail="User already exists!")
     users_assistance[assistance.id] = assistance
     return users_assistance[assistance.id]
 
@@ -139,7 +139,7 @@ def create_user_assistance(assistance: UserAssistance):
 def update_user_assistance(item_id: str, user: UpdateUserAssistance):
     
     if item_id not in users_assistance:
-        raise HTTPException(status_code=404, description="User not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
  
     if user.confirmationDetails != None:
         users_assistance[item_id].confirmationDetails = user.confirmationDetails     
@@ -152,7 +152,7 @@ def update_user_assistance(item_id: str, user: UpdateUserAssistance):
 @app.delete("/delete-user-assistance")
 def delete_user_assistance(item_id:str = Query(..., description = "The id item to delete.")):
     if item_id not in users_assistance: 
-        raise HTTPException(status_code=404, description="User not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
 
     del users_assistance[item_id]  
     return {"Success":"Successfully Deleted!"} 
@@ -187,12 +187,12 @@ def get_help_request_by_id(id:str, client_id:str):
     for item_id in help_request: 
         if help_request[item_id].id == id and help_request[item_id].client_id == client_id:
             return help_request[item_id]
-    raise HTTPException(status_code=404, description="User not found.")         
+    raise HTTPException(status_code=404, detail="User not found.")         
 
 @app.post("/create-help-request")
 def create_help_request(request: HelpRequest):
     if request.id in help_request:
-        raise HTTPException(status_code=409, description="Item ID already exists.")
+        raise HTTPException(status_code=409, detail="Item ID already exists.")
     help_request[request.id] = request    
     return help_request[request.id]
 
@@ -200,7 +200,7 @@ def create_help_request(request: HelpRequest):
 def update_help_request(item_id:str, request: UpdateHelpRequest):
 
     if item_id not in help_request:
-        raise HTTPException(status_code=404, description="Item does not exist.")
+        raise HTTPException(status_code=404, detail="Item does not exist.")
 
     if request.client_id != None:
         help_request[item_id].client_id = request.client_id
@@ -213,7 +213,7 @@ def update_help_request(item_id:str, request: UpdateHelpRequest):
 @app.delete("delete-help-request")
 def delete_help_request(item_id:str = Query(..., description = "The id item to delete.")):
     if item_id not in help_request:
-        raise HTTPException(status_code=404, description="Item does not exist.")
+        raise HTTPException(status_code=404, detail="Item does not exist.")
 
     del help_request[item_id]
     return {"Success":"Successfully Deleted."}    
@@ -246,15 +246,15 @@ def get_respondents_by_id(id:str):
     for item_id in respondents:
         if respondents[item_id].id == id:
             return respondents[item_id]
-    raise HTTPException(status_code=404, description="User not found")
+    raise HTTPException(status_code=404, detail="User not found")
 
 @app.post("/create-response")
 def create_respondent(response: Response):
     if response.id in respondents:
-        raise HTTPException(status_code=409, description="Item already exists.")
+        raise HTTPException(status_code=409, detail="Item already exists.")
 
     if response.id in response.respondents:
-        raise HTTPException(status_code=409, description="You can't be a respondent to your own request.")
+        raise HTTPException(status_code=409, detail="You can't be a respondent to your own request.")
 
     respondents[response.id] = response
     return respondents[response.id]
@@ -264,7 +264,7 @@ def create_respondent(response: Response):
 def update_respondent(item_id:str, response: UpdateResponse):
     
     if item_id not in respondents:
-        raise HTTPException(status_code=404, description="User not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
 
     if response.respondents != None: 
         respondents[item_id].respondents = response.respondents       
@@ -274,7 +274,7 @@ def update_respondent(item_id:str, response: UpdateResponse):
 @app.delete("/delete-response")
 def delete_response(item_id:str = Query(..., description = "The id item to delete.")):
     if item_id not in respondents:
-        raise HTTPException(status_code=404, description="User not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
 
     del respondents[item_id]
     return {"Success":"Successfully Deleted."}
@@ -308,12 +308,12 @@ def get_cancellations(id:str, clientId:str):
     for item_id in cancellations:
         if cancellations[item_id].id == id and cancellations[item_id].client_id == clientId:
             return cancellations[item_id]
-    raise HTTPException(status_code=404,description="User not found.")
+    raise HTTPException(status_code=404,detail="User not found.")
     
 @app.post("/create-cancellation-reason")
 def create_cancellation(cancellation: Cancellation):
     if cancellation.id in cancellations:
-        raise HTTPException(status_code=409,description="Item already exist.")
+        raise HTTPException(status_code=409,detail="Item already exist.")
 
     cancellations[cancellation.id] = cancellation    
     return cancellations[cancellation.id]
@@ -321,7 +321,7 @@ def create_cancellation(cancellation: Cancellation):
 @app.put("/update-cancellation-reason")
 def update_cancellation_reason(item_id:str, cancellation: UpdateCancellation):
     if item_id not in cancellations:
-        raise HTTPException(status_code=404,description="User not found.")
+        raise HTTPException(status_code=404,detail="User not found.")
 
     if cancellation.client_id != None:
         cancellations[item_id].client_id = cancellation.client_id
@@ -334,7 +334,7 @@ def update_cancellation_reason(item_id:str, cancellation: UpdateCancellation):
 @app.delete("/delete-cancellation-reason")
 def delete_response(item_id:str = Query(..., description = "The id item to delete.")):
     if item_id not in cancellations:
-        raise HTTPException(status_code=404,description="Item does not exist.")
+        raise HTTPException(status_code=404,detail="Item does not exist.")
 
     del cancellations[item_id]
     return {"Success":"Successfully Deleted."}
